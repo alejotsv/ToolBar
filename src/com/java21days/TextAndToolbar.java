172a347ae8b2f2e61ab42574aa07a096c481125e
Alejandro Salas <asalas.wd@outlook.com>
2022-11-13 18:57:06 -0500

    Set JFrame size and position in screen

---
+++ src/com/java21days/TextAndToolbar.java
@@ -0,0 +1,9 @@
+package com.java21days;
+
+public class TextAndToolbar {
+    public static void main(String[] args) {
+        String[] buttons = { "cancel", "delete", "download", "upload"};
+        ToolBar toolBar = new ToolBar("FeedBar", buttons);
+    }
+}
+
--- src/com/java21days/ToolBar.java
+++ src/com/java21days/ToolBar.java
@@ -1,18 +1,20 @@
 package com.java21days;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class ToolBar extends JFrame {
+    Dimension dim = new Dimension(300, 300);
 
     ToolBar(String title, String[] icons){
         super(title);
         JPanel panel = new JPanel();
+        setSize(dim);
 
-
+        setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         add(panel);
         setVisible(true);
-        pack();
 
     }
 }
