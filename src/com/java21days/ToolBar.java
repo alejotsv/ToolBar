a4189dabee2af9a4b57aaacb19b70a7d8a4c6fb2
Alejandro Salas <asalas.wd@outlook.com>
2022-11-13 18:53:12 -0500

    Generate basic constructor for ToolBar

--- src/com/java21days/ToolBar.java
+++ src/com/java21days/ToolBar.java
@@ -4,4 +4,15 @@ import javax.swing.*;
 
 public class ToolBar extends JFrame {
 
+    ToolBar(String title, String[] icons){
+        super(title);
+        JPanel panel = new JPanel();
+
+
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        add(panel);
+        setVisible(true);
+        pack();
+
+    }
 }
