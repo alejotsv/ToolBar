43a55013aad9295cf7d4e200c8726d18eb6a37cc
Alejandro Salas <asalas.wd@outlook.com>
2022-11-13 19:06:24 -0500

    Add scroll panel with textarea

--- src/com/java21days/ToolBar.java
+++ src/com/java21days/ToolBar.java
@@ -10,11 +10,19 @@ public class ToolBar extends JFrame {
         super(title);
         JPanel panel = new JPanel();
         setSize(dim);
+        JTextArea textArea = new JTextArea(10, 40);
+        JScrollPane scrollPane = new JScrollPane(
+                textArea,
+                ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED,
+                ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED
+        );
+        panel.add(scrollPane);
 
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         add(panel);
         setVisible(true);
-
+        pack();
     }
+
 }
